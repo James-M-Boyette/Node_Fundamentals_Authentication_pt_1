e6be0fd72b7f0b4908aa2562183b2b2db9a34b59
sou7hernsaint <James.M.84.Boyette@gmail.com>
2022-05-11 21:13:07 -0400

    Add Database pushing of User Records

--- src/accounts/register.js
+++ src/accounts/register.js
@@ -1,19 +1,28 @@
 import bcrypt from 'bcryptjs';
 const { genSalt, hash } = bcrypt;
 
+// import { user } from "../user/user.js"; 
+// The above threw an ?order of execution? error for Scott, so he dynamically imported it within the 'registerUser' function
 
 export async function registerUser(email, password) {
+    const { user } = await import("../user/user.js") // Dynamically-importing rather than at the beginning of the file
+
     // generate salt
     const salt = await genSalt(10); // Specifies how long the salt should be
-    console.log("salt:", salt)
 
     // hash with salt
     const hashedPassword = await hash(password, salt)
-    console.log("hashedPassword:", hashedPassword)
 
     // store in DB
+    const result = await user.insertOne({
+        email: {
+            address: email,
+            verified: false, // Future-proofing by anticipating company's future need to add "verify email" step in user sign-up
+        },
+        password: hashedPassword,
+    })
 
     // return user from DB
-    console.log("email:", email)
+    return result.insertedId
 
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-// "Initial Setup Tests"
-// console.log('Node Auth is running')
-// import "./db.js";
-
 // "Enivronment Variables"
 import "./env.js" // "Loads & runs file immediately"; clarify later
 
@@ -9,17 +5,20 @@ import "./env.js" // "Loads & runs file immediately"; clarify later
 import { fastify } from "fastify";
 
 // "Static File Directory Plugin"
-// import fastifyStatic from "fastify-static"; // Depricated ...
 import fastifyStatic from "@fastify/static";
 
 // "ESM Work-Arounds for __dirname"
 import path from "path";
 import { fileURLToPath } from "url";
 
+// "Salted user credentials"
+import { registerUser } from "./accounts/register.js"; 
+
 // "Database connection"
 import { connectDB } from "./db.js"
 
-import { registerUser } from "./accounts/register.js"; 
+
+
 
 // "ESM-specific syntax requirements for accessing static files"
 const __filename = fileURLToPath(import.meta.url) // get metadata about files
@@ -29,9 +28,6 @@ const __dirname = path.dirname(__filename)
 const app = fastify();
 const port = 3000;
 
-// "ENV structuring"
-// console.log(process.env.MONGO_URL)
-
 async function startApp(){
     try {
         // "Root Directory"
@@ -40,21 +36,16 @@ async function startApp(){
         })
 
         // "Routes"
-        // Note: these override any native routing behavior, so, we've commented-out the basic GET in order to serve the index.html automatically ...
-        // app.get('/', {}, (request, reply) => {
-        //     reply.send({
-        //         data: "Hello World!"
-        //     })
-        // })
-
         app.post('/api/register', {}, async (request, reply) => {
-            // console.log('request:', 'email:', request.body.email, 'password:', request.body.password);
             try {
-                await registerUser(request.body.email, request.body.password);
+                const userId = await registerUser(
+                    request.body.email, 
+                    request.body.password
+                    );
+                console.log('userId:', userId);
             } catch (e) {
                 console.error(e)
             }
-            
             reply.send({
                 data: "Hello World!"
             })
